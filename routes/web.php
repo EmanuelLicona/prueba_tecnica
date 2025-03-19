285b5527e60d076ef2772b612e1248a3380c3daf
EmanuelLicona <emanuel.licona@gmail.com>
2025-03-18 21:55:51 -0600

    Agregando formulario de registro

--- routes/web.php
+++ routes/web.php
@@ -4,12 +4,15 @@ use App\Http\Controllers\CategoryController;
 use App\Http\Controllers\ProductController;
 use App\Http\Controllers\UserController;
 use App\Http\Controllers\HomeController;
-
+use App\Http\Controllers\RegisterController;
 use Illuminate\Support\Facades\Route;
 
 Route::view('/login', 'login')->name('login');
 Route::get('/register', [UserController::class, 'register'])->name('register');
 Route::post('/executeRegister', [UserController::class, 'executeRegister'])->name('executeRegister');
+
+// registrar todas las rutas con una linea
+Route::resource('/register-form', RegisterController::class);
 
 Route::middleware('auth')->group(function () {
   Route::get('logout', [UserController::class, 'logout'])->name('logout');
