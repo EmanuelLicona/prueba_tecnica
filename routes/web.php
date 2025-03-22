2c3a79d7a9a53cb99a16bb2da76d86d1c10c597b
EmanuelLicona <emanuel.licona@gmail.com>
2025-03-21 23:18:17 -0600

    Cambio de rutas

--- routes/web.php
+++ routes/web.php
@@ -7,14 +7,14 @@ use Illuminate\Support\Facades\Route;
 
 Route::view('/login', 'login')->name('login');
 // Route::get('/register', [UserController::class, 'register'])->name('register');
+// Route::post('/executeRegister', [UserController::class, 'executeRegister'])->name('executeRegister');
 
-Route::post('/executeRegister', [UserController::class, 'executeRegister'])->name('executeRegister');
+Route::get('/register-form', [RegisterController::class, 'form'])->name('register_form');
 Route::resource('/register', RegisterController::class);
 
 Route::middleware('auth')->group(function () {
+  
   Route::get('logout', [UserController::class, 'logout'])->name('logout');
-  Route::get('/register-form', [RegisterController::class, 'form'])->name('register_form');
-
 
   Route::get('/', [HomeController::class, 'index'])->name('index');
   Route::get('/user', [UserController::class, 'index'])->name('user.index');
